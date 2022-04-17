1c037a783f7c6414b6fba376250b628f0b528510
Amit Raturi <amit.raturi@soroco.com>
2022-04-17 23:01:52 +0530

    Setting up the gin and a http server

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"golang_workshop/facade"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	input := "The quick brown fox jumped over the lazy dog"
-	rev := facade.Reverse(input)
-	doubleRev := facade.Reverse(rev)
-	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q\n", rev)
-	fmt.Printf("reversed again: %q\n", doubleRev)
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := router.Run()
+	if err != nil {
+		return
+	}
 }
