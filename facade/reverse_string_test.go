c957f6aeb30dd22efce7a2e6f260aba57697d755
Manthan Sharma <manthan.sharma@soroco.com>
2022-04-17 22:41:45 +0530

    Add UTF8 Checks

--- facade/reverse_string_test.go
+++ facade/reverse_string_test.go
@@ -11,9 +11,14 @@ func FuzzReverse(f *testing.F) {
 		f.Add(tc) // Use f.Add to provide a seed corpus
 	}
 	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
-		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
 		if orig != doubleRev {
 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := facade.Reverse(input)
-	doubleRev := facade.Reverse(rev)
+	rev, revErr := facade.Reverse(input)
+	doubleRev, doubleRevErr := facade.Reverse(rev)
 	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q\n", rev)
-	fmt.Printf("reversed again: %q\n", doubleRev)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
 }
