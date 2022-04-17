a2758c0a7963c6de7977e272da8c9b55a04a697a
Amit Raturi <amit.raturi@soroco.com>
2022-04-17 23:28:44 +0530

    Reverse API

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang_workshop/facade"
+	"net/http"
 )
+
+type ReverseAPIInput struct {
+	Text string
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+	router.POST("/reverse", func(c *gin.Context) {
+		var apiInput ReverseAPIInput
+		if err := c.BindJSON(&apiInput); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"reverse": "",
+				"message": "Invalid input",
+			})
+		}
+		rev := facade.Reverse(apiInput.Text)
+		c.JSON(http.StatusOK, gin.H{
+			"reverse": rev,
+			"message": "",
 		})
 	})
 	err := router.Run()
